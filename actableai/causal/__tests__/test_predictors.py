0d9d543ed91272959528e1be39b08a612f77635c
MehdiActable <mehdib@actable.ai>
2022-04-05 15:31:57 +0200

    feat: Adding tests for SKLearnWrapper and DataFrameTransformer

---
+++ actableai/causal/__tests__/test_predictors.py
@@ -0,0 +1,31 @@
+import numpy as np
+import pandas as pd
+from autogluon.tabular import TabularPredictor
+
+from actableai.causal.predictors import DataFrameTransformer, SKLearnWrapper
+
+class TestDataFrameTransformer:
+    def test_fit_transform(self):
+        dft = DataFrameTransformer()
+        df = dft.fit_transform(
+            np.array([[0, 1, 2]])
+        )
+        assert isinstance(df, pd.DataFrame)
+        assert df.shape == (1, 3)
+
+    def test_fit_transform_col_names(self):
+        dft = DataFrameTransformer()
+        df = dft.fit_transform(
+            np.array([[0, 1, 2]]), column_name=['a']
+        )
+        assert isinstance(df, pd.DataFrame)
+        assert df.shape == (1, 3)
+        assert list(df.columns) == ['a']
+
+class TestSKLearnWrapper:
+    def test_init(self):
+        sklw = SKLearnWrapper(TabularPredictor('y'), ['a', 'b'], None, "best_quality", None)
+        assert sklw.ag_args_fit is None
+        assert sklw.ag_predictor is None
+        assert sklw.presets == "best_quality"
+        assert TabularPredictor is not None
