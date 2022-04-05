fix: Fixing the name of important features causal inference

actableai/causal/models.py
@@ -147,14 +147,13 @@ class AAICausalEstimator:
         if cv == "auto":
             cv = 5
 
-        XW = W if X is None else X if W is None else np.hstack([X, W])
         model_t = TabularPredictor(
             path=random_directory(model_directory),
             label=label_t,
             problem_type="multiclass" if self.has_categorical_treatment else "regression",
         )
         model_t = SKLearnWrapper(
-            XW, model_t, hyperparameters=hyperparameters, presets=presets,
+            model_t, list(X.columns) + list(W.columns), hyperparameters=hyperparameters, presets=presets,
             ag_args_fit={
                 "num_gpus": num_gpus,
             }
@@ -165,7 +164,7 @@ class AAICausalEstimator:
             problem_type="binary" if self.has_binary_outcome else "regression",
         )
         model_y = SKLearnWrapper(
-            XW, model_y, hyperparameters=hyperparameters, presets=presets,
+            model_y, list(X.columns) + list(W.columns), hyperparameters=hyperparameters, presets=presets,
             ag_args_fit = {
                 "num_gpus": num_gpus,
             }

actableai/causal/predictors.py
@@ -20,25 +20,26 @@ class UnsupportedProblemType(ValueError):
     pass
 
 
-class DataFrameTransformer(BaseEstimator, TransformerMixin):
-    def __init__(self, X):
-        self._fitted = False
-        if X is not None and type(X) is pd.DataFrame:
-            self._fitted = True
-            self.columns = list(X.columns)
-            self.dtypes = X.dtypes
+class DataFrameTransformer(TransformerMixin):
+    """DataFrame Transformer to convert List to DataFrame
 
-    def fit(self, X, y=None):
-        return self
-
-    def transform(self, X, y=None):
-        if not self._fitted:
-            return pd.DataFrame(X)
-
-        X = pd.DataFrame(X, columns=self.columns)
-        for col, dtype in zip(X, self.dtypes):
-            X[col] = X[col].astype(dtype)
-        return X
+    Args:
+        TransformerMixin (_type_): Base Class Transformer of SKLearn
+    """
+    def fit_transform(self, X, y=None, x_w_columns=None, **fit_params):
+        if isinstance(X, np.ndarray):
+            df = pd.DataFrame(X.tolist())
+            if x_w_columns is not None:
+                df.columns = x_w_columns
+            return df
+        if isinstance(X, list) and len(np.array(X).shape) != 2:
+            raise Exception("List must be two dimensional to be converted to DataFrame")
+        if isinstance(X, list) or isinstance(X, dict):
+            df = pd.DataFrame(X)
+            if x_w_columns is not None:
+                df.columns = x_w_columns
+            return df
+        raise TypeError
 
 
 class LinearRegressionWrapper(LinearRegression):
@@ -56,11 +57,11 @@ class LinearRegressionWrapper(LinearRegression):
 class SKLearnWrapper:
     def __init__(
         self,
-        df: pd.DataFrame,
         ag_predictor: TabularPredictor,
+        x_w_columns,
         hyperparameters=None,
         presets="best_quality",
-        ag_args_fit=None,
+        ag_args_fit=None
     ):
         """Construct a sklearn wrapper object
 
@@ -76,12 +77,15 @@ class SKLearnWrapper:
         else:
             self.hyperparameters = hyperparameters
         self.presets = presets
-        self._df_transformer = DataFrameTransformer(df)
+        self._df_transformer = DataFrameTransformer()
         self.ag_args_fit = ag_args_fit
+        self.train_data = None
+        self.x_w_columns=x_w_columns
 
     def fit(self, X, y, sample_weight=None):
         label = self.ag_predictor.label
-        train_data = self._df_transformer.transform(X)
+        train_data = self._df_transformer.fit_transform(X, x_w_columns=self.x_w_columns)
+        train_data.columns = self.x_w_columns
         train_data[label] = y
         train_data = TabularDataset(train_data)
         self.ag_predictor.sample_weight = sample_weight
@@ -95,21 +99,23 @@ class SKLearnWrapper:
         self.train_data = train_data
 
     def feature_importance(self):
+        if self.train_data is None:
+            raise Exception("The predictor needs to be fitted before")
         return self.ag_predictor.feature_importance(self.train_data)
 
     def predict(self, X):
-        test_data = self._df_transformer.transform(X)
+        test_data = self._df_transformer.fit_transform(X, x_w_columns=self.x_w_columns)
         test_data = TabularDataset(test_data)
         y_pred = self.ag_predictor.predict(test_data).values
         return y_pred
 
     def predict_proba(self, X):
-        test_data = self._df_transformer.transform(X)
+        test_data = self._df_transformer.fit_transform(X, x_w_columns=self.x_w_columns)
         y_pred_proba = self.ag_predictor.predict_proba(test_data)
         return y_pred_proba.values
 
     def score(self, X, y, sample_weight=None):
-        test_data = self._df_transformer.transform(X)
+        test_data = self._df_transformer.fit_transform(X, x_w_columns=self.x_w_columns)
         y_pred = self.ag_predictor.predict(test_data).values
         if self.ag_predictor.problem_type in ["binary", "multiclass"]:
             return {