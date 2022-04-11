aa402b915afb108c8d8f21074d6681be91e05b38
DemyCode <verycols@gmail.com>
2022-04-11 18:42:30 +0200

    fix: Fixing tests for SKLearnWrapper

--- actableai/causal/__tests__/test_predictors.py
+++ actableai/causal/__tests__/test_predictors.py
@@ -25,15 +25,15 @@ class TestDataFrameTransformer:
 
 class TestSKLearnWrapper:
     def test_init(self):
-        sklw = SKLearnWrapper(TabularPredictor('y'), ['a', 'b'], None, "best_quality", None)
+        sklw = SKLearnWrapper(TabularPredictor('y'), ['a', 'b'], None, "medium_quality_faster_train", None)
         assert sklw.ag_args_fit is None
         assert sklw.ag_predictor is not None
-        assert sklw.presets == "best_quality"
+        assert sklw.presets == "medium_quality_faster_train"
         assert TabularPredictor is not None
 
     def test_fit(self):
         width, height = 10, 20
-        sklw = SKLearnWrapper(TabularPredictor('y'), [str(i) for i in range(width)], None, "best_quality", None)
+        sklw = SKLearnWrapper(TabularPredictor('y'), [str(i) for i in range(width)], None, "medium_quality_faster_train", None)
         X = np.random.randint(0, 100, size=(height, width))
         y = np.array(np.random.randint(0, 100, size=(height, 1)))
         sklw.fit(X, y)
@@ -42,16 +42,16 @@ class TestSKLearnWrapper:
 
     def test_feature_importance(self):
         width, height = 10, 20
-        sklw = SKLearnWrapper(TabularPredictor('y'), [str(i) for i in range(width)], None, "best_quality", None)
+        sklw = SKLearnWrapper(TabularPredictor('y'), [str(i) for i in range(width)], None, "medium_quality_faster_train", None)
         X = np.random.randint(0, 100, size=(height, width))
         y = np.array(np.random.randint(0, 100, size=(height, 1)))
         sklw.fit(X, y)
         feat_imp = sklw.feature_importance()
         assert sklw.ag_predictor.sample_weight is None
         assert sklw.train_data is not None
-        assert set(feat_imp.index) == set(['a', 'b', 'c'])
+        assert set(feat_imp.index) == set([str(i) for i in range(width)])
 
     def test_feature_importance_no_fit(self):
-        sklw = SKLearnWrapper(TabularPredictor('y'), ['a', 'b', 'c'], None, "best_quality", None)
+        sklw = SKLearnWrapper(TabularPredictor('y'), ['a', 'b', 'c'], None, "medium_quality_faster_train", None)
         with pytest.raises(Exception):
             sklw.feature_importance()
