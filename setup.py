38160d6fd271fbcf639fbf72608d9926e9a0c9c0
DemyCode <verycols@gmail.com>
2022-04-13 21:49:21 +0200

    fix: Integrating nltk download on pip install

--- setup.py
+++ setup.py
@@ -2,27 +2,50 @@ import os
 import numpy as np
 from setuptools import Extension, setup, find_packages
 from setuptools.command.install import install
+from setuptools.command.develop import develop
+from setuptools.command.egg_info import egg_info
 
 libraries = []
-if os.name == 'posix':
-    libraries.append('m')
+if os.name == "posix":
+    libraries.append("m")
 
 extensions = []
-for name in ['_tree', '_splitter', '_criterion', '_utils']:
-    extensions.append(Extension(
-        'actableai.third_parties.skgarden.mondrian.tree.{}'.format(name),
-        sources=['actableai/third_parties/skgarden/mondrian/tree/{}.pyx'.format(name)],
-        include_dirs=[np.get_include()],
-        libraries=libraries,
-        extra_compile_args=['-O3'],
-    ))
+for name in ["_tree", "_splitter", "_criterion", "_utils"]:
+    extensions.append(
+        Extension(
+            "actableai.third_parties.skgarden.mondrian.tree.{}".format(name),
+            sources=[
+                "actableai/third_parties/skgarden/mondrian/tree/{}.pyx".format(name)
+            ],
+            include_dirs=[np.get_include()],
+            libraries=libraries,
+            extra_compile_args=["-O3"],
+        )
+    )
 
 
-class DownloadNLTK(install):
+class CustomInstallCommand(install):
     def run(self):
-        super().do_egg_install()
+        install.run(self)
         import nltk
-        nltk.download('stopwords')
+
+        nltk.download("stopwords")
+
+
+class CustomDevelopCommand(develop):
+    def run(self):
+        develop.run(self)
+        import nltk
+
+        nltk.download("stopwords")
+
+
+class CustomEggInfoCommand(egg_info):
+    def run(self):
+        egg_info.run(self)
+        import nltk
+
+        nltk.download("stopwords")
 
 
 setup(
@@ -38,17 +61,14 @@ setup(
         "autogluon.text @ file://./third_parties/autogluon/text",
         "pandasql>=0.7.3",
         "fastai>=2.3.1",
-
         # gluonts
         "matplotlib>=3.0",
         "pydantic>=1.8",
         "ujson>=1.35",
-
         # pts
         "scipy>=1.3.3,<1.7.0",
         "python-rapidjson>=1.0",
         "torch>=1.7.1",
-
         "click==7.1.1",
         "scikit-learn>=0.24.2,<0.25",
         "keras<2.5.0",
@@ -64,13 +84,11 @@ setup(
         "sqlalchemy>=1.3.5, <2.0",
         "redis>=3.5.3",
         "filelock>=3.0.12",
-
         # causal
         "econml>=0.11.1",
         "dowhy>=0.6",
         "networkx>=2.5.1",
-
-        "nltk"
+        "nltk",
     ],
     setup_requires=[
         "cython>=0.23",
@@ -82,11 +100,13 @@ setup(
         "pystan>=2.19.1.1",
         "tqdm>=4.23",
         "wheel",
-        "nltk"
+        "nltk",
     ],
     include_package_data=True,
     ext_modules=extensions,
     cmdclass={
-        DownloadNLTK.__name__: DownloadNLTK
-    }
+        "install": CustomInstallCommand,
+        "develop": CustomDevelopCommand,
+        "egg_info": CustomEggInfoCommand,
+    },
 )
