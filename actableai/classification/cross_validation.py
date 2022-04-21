2c0d2c4ba4c1360d57e170a0abfc56da13f71be2
DemyCode <verycols@gmail.com>
2022-04-21 15:06:46 +0200

    fix: precision_recall_curve for classification

--- actableai/classification/cross_validation.py
+++ actableai/classification/cross_validation.py
@@ -4,6 +4,7 @@ import pandas as pd
 from functools import reduce
 from actableai.classification.utils import leaderboard_cross_val
 
+
 class AverageEnsembleClassifier:
     """
     TODO write documentation
@@ -36,15 +37,11 @@ class AverageEnsembleClassifier:
 
         if len(self.class_labels) == 2:
             pred_labels = [
-                self.class_labels[0]
-                if x < 0.5 else self.class_labels[1]
+                self.class_labels[0] if x < 0.5 else self.class_labels[1]
                 for x in pred_probas
             ]
         else:
-            pred_labels = [
-                self.class_labels[np.argmax(x)]
-                for x in pred_probas
-            ]
+            pred_labels = [self.class_labels[np.argmax(x)] for x in pred_probas]
 
         return pd.Series(pred_labels)
 
@@ -63,23 +60,25 @@ class AverageEnsembleClassifier:
             predictor.unpersist_models()
 
 
-def run_cross_validation(classification_train_task,
-                         problem_type,
-                         positive_label,
-                         presets,
-                         hyperparameters,
-                         model_directory,
-                         target,
-                         features,
-                         df_train,
-                         kfolds,
-                         cross_validation_max_concurrency,
-                         drop_duplicates,
-                         run_debiasing,
-                         biased_groups,
-                         debiased_features,
-                         residuals_hyperparameters,
-                         num_gpus):
+def run_cross_validation(
+    classification_train_task,
+    problem_type,
+    positive_label,
+    presets,
+    hyperparameters,
+    model_directory,
+    target,
+    features,
+    df_train,
+    kfolds,
+    cross_validation_max_concurrency,
+    drop_duplicates,
+    run_debiasing,
+    biased_groups,
+    debiased_features,
+    residuals_hyperparameters,
+    num_gpus,
+):
     """
     TODO write documentation
     """
@@ -104,7 +103,9 @@ def run_cross_validation(classification_train_task,
                 "positive_label": positive_label,
                 "presets": presets,
                 "hyperparameters": hyperparameters,
-                "model_directory": os.path.join(model_directory, f"trainer_{kfold_index}"),
+                "model_directory": os.path.join(
+                    model_directory, f"trainer_{kfold_index}"
+                ),
                 "target": target,
                 "features": features,
                 "df_train": df_train.iloc[train_index],
@@ -115,15 +116,12 @@ def run_cross_validation(classification_train_task,
                 "debiased_features": debiased_features,
                 "residuals_hyperparameters": residuals_hyperparameters,
                 "num_gpus": num_gpus,
-            }
+            },
         )
         for kfold_index, (train_index, val_index) in enumerate(kfolds_index_list)
     ]
 
-    cross_val_results = [
-        results.get()
-        for results in cross_val_async_results
-    ]
+    cross_val_results = [results.get() for results in cross_val_async_results]
 
     kfold_pool.close()
 
@@ -133,11 +131,18 @@ def run_cross_validation(classification_train_task,
     cross_val_important_features = {}
     cross_val_evaluates = {}
     cross_val_auc_curves = {}
+    cross_val_precision_recall_curves = {}
     df_val_cross_val_pred_prob = []
 
     df_val = pd.DataFrame()
 
-    for kfold_index, (predictor, important_features, evaluate, df_val_pred_prob, leaderboard) in enumerate(cross_val_results):
+    for kfold_index, (
+        predictor,
+        important_features,
+        evaluate,
+        df_val_pred_prob,
+        leaderboard,
+    ) in enumerate(cross_val_results):
         _, val_index = kfolds_index_list[kfold_index]
 
         df_k_val = df_train.iloc[val_index].copy()
@@ -151,7 +156,9 @@ def run_cross_validation(classification_train_task,
             if feature["feature"] not in cross_val_important_features:
                 cross_val_important_features[feature["feature"]] = []
 
-            cross_val_important_features[feature["feature"]].append(feature["importance"])
+            cross_val_important_features[feature["feature"]].append(
+                feature["importance"]
+            )
 
         for metric in evaluate:
             if metric not in cross_val_evaluates:
@@ -164,50 +171,102 @@ def run_cross_validation(classification_train_task,
             for metric in auc_curve:
                 if metric not in cross_val_evaluates:
                     cross_val_auc_curves[metric] = []
-
                 cross_val_auc_curves[metric].append(auc_curve[metric])
 
-        df_val_cross_val_pred_prob.append(json.loads(df_val_pred_prob.to_json(orient="table"))["data"])
+            precision_recall_curve = evaluate["precision_recall_curve"]
+            for metric in precision_recall_curve:
+                if metric not in cross_val_evaluates:
+                    cross_val_precision_recall_curves[metric] = []
+                cross_val_precision_recall_curves[metric].append(
+                    precision_recall_curve[metric]
+                )
+
+        df_val_cross_val_pred_prob.append(
+            json.loads(df_val_pred_prob.to_json(orient="table"))["data"]
+        )
 
     # Evaluate results
     sqrt_k = math.sqrt(kfolds)
     important_features = []
     for k in cross_val_important_features.keys():
-        important_features.append({
-            "feature": k,
-            "importance": np.mean(cross_val_important_features[k]),
-            "importance_std_err": np.std(cross_val_important_features[k]) / sqrt_k
-        })
-    important_features = sorted(important_features, key=lambda k: k["importance"], reverse=True)
+        important_features.append(
+            {
+                "feature": k,
+                "importance": np.mean(cross_val_important_features[k]),
+                "importance_std_err": np.std(cross_val_important_features[k]) / sqrt_k,
+            }
+        )
+    important_features = sorted(
+        important_features, key=lambda k: k["importance"], reverse=True
+    )
 
     evaluate = {
         "problem_type": cross_val_evaluates["problem_type"][0],
         "labels": [str(x) for x in cross_val_evaluates["labels"][0]],
         "accuracy": np.mean(cross_val_evaluates["accuracy"], axis=0),
         "accuracy_std_err": np.std(cross_val_evaluates["accuracy"], axis=0) / sqrt_k,
-        "confusion_matrix": np.mean(cross_val_evaluates["confusion_matrix"], axis=0).tolist(),
-        "confusion_matrix_std_err": (np.std(cross_val_evaluates["confusion_matrix"], axis=0) / sqrt_k).tolist()
+        "confusion_matrix": np.mean(
+            cross_val_evaluates["confusion_matrix"], axis=0
+        ).tolist(),
+        "confusion_matrix_std_err": (
+            np.std(cross_val_evaluates["confusion_matrix"], axis=0) / sqrt_k
+        ).tolist(),
     }
 
     if evaluate["problem_type"] == "binary":
         auc_curve = {
-            "False Positive Rate": np.mean(cross_val_auc_curves["False Positive Rate"], axis=0).tolist(),
+            "False Positive Rate": np.mean(
+                cross_val_auc_curves["False Positive Rate"], axis=0
+            ).tolist(),
             "False Positive Rate std err": (
-                    np.std(cross_val_auc_curves["False Positive Rate"], axis=0) / sqrt_k).tolist(),
-            "True Positive Rate": np.mean(cross_val_auc_curves["True Positive Rate"], axis=0).tolist(),
+                np.std(cross_val_auc_curves["False Positive Rate"], axis=0) / sqrt_k
+            ).tolist(),
+            "True Positive Rate": np.mean(
+                cross_val_auc_curves["True Positive Rate"], axis=0
+            ).tolist(),
             "True Positive Rate std err": (
-                    np.std(cross_val_auc_curves["True Positive Rate"], axis=0) / sqrt_k).tolist(),
+                np.std(cross_val_auc_curves["True Positive Rate"], axis=0) / sqrt_k
+            ).tolist(),
             "thresholds": np.mean(cross_val_auc_curves["thresholds"], axis=0).tolist(),
-            "thresholds_std_err": (np.std(cross_val_auc_curves["thresholds"], axis=0) / sqrt_k).tolist(),
+            "thresholds_std_err": (
+                np.std(cross_val_auc_curves["thresholds"], axis=0) / sqrt_k
+            ).tolist(),
             "positive_label": cross_val_auc_curves["positive_label"][0],
             "negative_label": cross_val_auc_curves["negative_label"][0],
-            "threshold": cross_val_auc_curves["threshold"][0]
+            "threshold": cross_val_auc_curves["threshold"][0],
+        }
+        precision_recall_curve = {
+            "Precision": np.mean(
+                cross_val_precision_recall_curves["Precision"], axis=0
+            ).tolist(),
+            "Precision std err": (
+                np.std(cross_val_precision_recall_curves["Precision"], axis=0) / sqrt_k
+            ).tolist(),
+            "Recall": np.mean(
+                cross_val_precision_recall_curves["Recall"], axis=0
+            ).tolist(),
+            "Recall std err": (
+                np.std(cross_val_precision_recall_curves["Recall"], axis=0) / sqrt_k
+            ).tolist(),
+            "thresholds": np.mean(
+                cross_val_precision_recall_curves["thresholds"], axis=0
+            ).tolist(),
+            "thresholds_std_err": (
+                np.std(cross_val_precision_recall_curves["thresholds"], axis=0) / sqrt_k
+            ).tolist(),
+            "positive_label": cross_val_precision_recall_curves["positive_label"][0],
+            "negative_label": cross_val_precision_recall_curves["negative_label"][0],
+            "threshold": cross_val_precision_recall_curves["threshold"][0],
         }
 
         evaluate["auc_score"] = np.mean(cross_val_evaluates["auc_score"], axis=0)
-        evaluate["auc_score_std_err"] = (np.std(cross_val_evaluates["auc_score"], axis=0) / sqrt_k).tolist()
+        evaluate["auc_score_std_err"] = (
+            np.std(cross_val_evaluates["auc_score"], axis=0) / sqrt_k
+        ).tolist()
         evaluate["auc_curve"] = auc_curve
-        evaluate["precision_score"] = np.mean(cross_val_evaluates["precision_score"], axis=0)
+        evaluate["precision_score"] = np.mean(
+            cross_val_evaluates["precision_score"], axis=0
+        )
         evaluate["recall_score"] = np.mean(cross_val_evaluates["recall_score"], axis=0)
         evaluate["f1_score"] = np.mean(cross_val_evaluates["f1_score"], axis=0)
 
@@ -216,4 +275,11 @@ def run_cross_validation(classification_train_task,
 
     leaderboard = leaderboard_cross_val(cross_val_leaderboard)
 
-    return ensemble_model, important_features, evaluate, df_val_cross_val_pred_prob, df_val, leaderboard
+    return (
+        ensemble_model,
+        important_features,
+        evaluate,
+        df_val_cross_val_pred_prob,
+        df_val,
+        leaderboard,
+    )
