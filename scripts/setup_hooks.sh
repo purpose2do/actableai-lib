4395866374d3726010a7635f154fb7126de10a4d
DemyCode <verycols@gmail.com>
2022-05-17 11:49:04 +0200

    fix: Fixing hooks

--- scripts/setup_hooks.sh
+++ scripts/setup_hooks.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+FILE="$PWD"/.git/hooks/pre-push
+if test -f "$FILE"; then
+    echo "$FILE exists removing old one"
+    rm "$FILE"
+fi
+
 chmod +x "$PWD"/scripts/pre-push
 ln -s "$PWD"/scripts/pre-push "$PWD"/.git/hooks/pre-push
 
