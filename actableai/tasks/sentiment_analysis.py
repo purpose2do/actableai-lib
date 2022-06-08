d77b5d83756f4ce8b849792a2c33ff8de090e795
Trung Huynh <trunghlt@gmail.com>
2022-06-08 15:00:58 +0200

    Update sentiment analysis task to use pyabsa

--- actableai/tasks/sentiment_analysis.py
+++ actableai/tasks/sentiment_analysis.py
@@ -26,59 +26,49 @@ class AAISentimentAnalysisTask(AAITask):
         Returns:
             Dict: Dictionnary of results
         """
-        try:
-            import math
-            import time
-            import traceback
-            import ray
-            from ray import serve
-            from nltk.tokenize import sent_tokenize
-            from actableai.sentiment.SpanABSA import AAISentimentExtractor
+        import math
+        import time
+        import traceback
+        import ray
+        from ray import serve
+        from nltk.tokenize import sent_tokenize
+        from actableai.sentiment.serve import AAISentimentExtractor
 
-            start = time.time()
+        start = time.time()
 
-            # To resolve any issues of acces rights make a copy
-            df = df.copy()
+        # To resolve any issues of acces rights make a copy
+        df = df.copy()
 
-            # Get serve handle
-            span_absa_handle = AAISentimentExtractor.get_handle()
+        # Get serve handle
+        absa_handle = AAISentimentExtractor.get_handle()
 
-            row_ids, sentences = [], []
-            for i, doc in enumerate(df[target].to_list()):
-                if doc is not None:
-                    for paragraph in doc.split("</br>"):
-                        for sent in sent_tokenize(paragraph):
-                            row_ids.append(i)
-                            sentences.append(sent)
+        row_ids, sentences = [], []
+        for i, doc in enumerate(df[target].to_list()):
+            if doc is not None:
+                for paragraph in doc.split("</br>"):
+                    for sent in sent_tokenize(paragraph):
+                        row_ids.append(i)
+                        sentences.append(sent)
 
-            # Call the deployed model batch by batch
-            result = []
-            for batch_index in range(math.ceil(len(sentences) / batch_size)):
-                sentence_batch = sentences[
-                    batch_index * batch_size : (batch_index + 1) * batch_size
-                ]
-                result += ray.get(
-                    span_absa_handle.options(method_name="predict").remote(
-                        sentence_batch
-                    )
+        # Call the deployed model batch by batch
+        result = []
+        for batch_index in range(math.ceil(len(sentences) / batch_size)):
+            sentence_batch = sentences[
+                batch_index * batch_size : (batch_index + 1) * batch_size
+            ]
+            result += ray.get(
+                absa_handle.options(method_name="predict").remote(sentence_batch))
+
+        data = []
+        for i, re in enumerate(result):
+            for kw, sentiment in zip(re["keyword"], re["sentiment"]):
+                data.append(
+                    {
+                        "keyword": kw,
+                        "sentiment": sentiment,
+                        "sentence": sentences[i],
+                        "row": row_ids[i],
+                    }
                 )
 
-            data = []
-            for i, re in enumerate(result):
-                for kw, sentiment in zip(re["keyword"], re["sentiment"]):
-                    data.append(
-                        {
-                            "keyword": kw,
-                            "sentiment": sentiment,
-                            "sentence": sentences[i],
-                            "row": row_ids[i],
-                        }
-                    )
-
-            return {"data": data, "status": "SUCCESS", "runtime": time.time() - start}
-        except Exception as err:
-            return {
-                "status": "FAILURE",
-                "message": str(err),
-                "trace": traceback.format_exc(),
-            }
+        return {"data": data, "status": "SUCCESS", "runtime": time.time() - start}
