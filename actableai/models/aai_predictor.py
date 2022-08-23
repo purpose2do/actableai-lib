57285c0d51b8585045481d047896efe55be0b734
DemyCode <verycols@gmail.com>
2022-08-23 14:08:53 +0200

    feat: Tabular Predictor Wrapper

---
+++ actableai/models/aai_predictor.py
@@ -0,0 +1,12 @@
+from typing import Optional
+from autogluon.tabular import TabularPredictor
+from econml.dml import DML
+
+
+class AAIPredictor:
+    def __init__(
+        self, version: int, predictor: TabularPredictor, causal_model: Optional[DML] = None
+    ):
+        self.version = version
+        self.predictor = predictor
+        self.causal_model = causal_model
