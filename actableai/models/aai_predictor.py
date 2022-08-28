f40082b781bd0f46e591a284c9ef5d59a3d67bb1
DemyCode <verycols@gmail.com>
2022-08-28 23:53:01 +0200

    fix: Custom marginal effect CATE + reversed effect

--- actableai/models/aai_predictor.py
+++ actableai/models/aai_predictor.py
@@ -29,15 +29,22 @@ class AAITabularModel(AAIModel):
 
     def intervention_effect(self, df: pd.DataFrame, pred: Dict) -> pd.DataFrame:
         if self.causal_model and self.intervened_column:
-            CME = self.causal_model.const_marginal_effect(
-                df[self.common_causes] if self.common_causes is not None else None
-            ).squeeze()
             ctr, cta = (
                 df[self.intervened_column].replace("", np.nan).astype(float),
                 pred["prediction"][self.predictor.label]
                 .replace("", np.nan)
                 .astype(float),
             )
+            custom_effect = []
+            for index_lab in range(len(df)):
+                curr_CME = self.causal_model.const_marginal_effect(
+                    df[self.common_causes][index_lab : index_lab + 1]
+                    if self.common_causes is not None and len(self.common_causes) != 0
+                    else None
+                )
+                curr_CME = curr_CME.squeeze()
+                custom_effect.append(curr_CME)
+            CME = np.array(custom_effect).squeeze()
             # New Outcome
             new_out = [None for _ in range(len(df))]
             if f"intervened_{self.intervened_column}" in df:
@@ -46,7 +53,7 @@ class AAITabularModel(AAIModel):
                     .replace("", np.nan)
                     .astype(float)
                 )
-                new_out = (ntr - ctr) * CME + cta
+                new_out = (ctr - ntr) * CME + cta
             # New Intervention
             new_inter = [None for _ in range(len(df))]
             if f"expected_{self.predictor.label}" in df:
@@ -55,7 +62,7 @@ class AAITabularModel(AAIModel):
                     .replace("", np.nan)
                     .astype(float)
                 )
-                new_inter = ((nta - cta) / CME) + ctr
+                new_inter = -((nta - cta) / CME) + ctr
             return pd.DataFrame(
                 {
                     f"expected_{self.predictor.label}": new_out,
