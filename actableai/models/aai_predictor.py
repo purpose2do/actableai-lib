fix: Reverting the ctr - ntr calculation

actableai/models/aai_predictor.py
@@ -53,7 +53,7 @@ class AAITabularModel(AAIModel):
                     .replace("", np.nan)
                     .astype(float)
                 )
-                new_out = (ctr - ntr) * CME + cta
+                new_out = (ntr - ctr) * CME + cta
             # New Intervention
             new_inter = [None for _ in range(len(df))]
             if f"expected_{self.predictor.label}" in df:
@@ -62,7 +62,7 @@ class AAITabularModel(AAIModel):
                     .replace("", np.nan)
                     .astype(float)
                 )
-                new_inter = -((nta - cta) / CME) + ctr
+                new_inter = ((nta - cta) / CME) + ctr
             return pd.DataFrame(
                 {
                     f"expected_{self.predictor.label}": new_out,

actableai/tasks/classification.py
@@ -480,7 +480,6 @@ class AAIClassificationTask(AAITask):
         from scipy.stats import spearmanr
         from sklearn.model_selection import train_test_split
         from autogluon.common.features.infer_types import check_if_nlp_feature
-        from autogluon.tabular import TabularPredictor
 
         from actableai.utils import (
             memory_efficient_hyperparameters,
@@ -848,13 +847,13 @@ class AAIClassificationTask(AAITask):
             predictor.refit_full(model="best", set_best_to_refit_full=True)
 
         model = None
-        if (kfolds <= 1 or refit_full) and isinstance(predictor, TabularPredictor):
+        if (kfolds <= 1 or refit_full) and predictor:
             model = AAITabularModel(
-                MODEL_DEPLOYMENT_VERSION,
-                predictor,
-                causal_model,
-                current_intervention_column,
-                common_causes,
+                version=MODEL_DEPLOYMENT_VERSION,
+                predictor=predictor,
+                causal_model=causal_model,
+                intervened_column=current_intervention_column,
+                common_causes=common_causes,
             )
 
         runtime = time.time() - start