fix: Fixing boolean bug + two intervention formula + DML Featurizer

actableai/models/intervention.py
@@ -3,7 +3,11 @@ import pandas as pd
 
 
 def empty_string_to_nan(
-    df: pd.DataFrame, task_model, intervention_col: str, new_intervention_col: str
+    df: pd.DataFrame,
+    task_model,
+    intervention_col: str,
+    new_intervention_col: str,
+    expected_target: str,
 ) -> pd.DataFrame:
     """Replace the empty string to nan in interventional model, also checks if the
         column exist in the DataFrame for new_intervention_col
@@ -23,6 +27,8 @@ def empty_string_to_nan(
     df[intervention_col] = df[intervention_col].replace("", np.nan)
     if new_intervention_col in df:
         df[new_intervention_col] = df[new_intervention_col].replace("", np.nan)
+    if expected_target in df:
+        df[expected_target] = df[expected_target].replace("", np.nan).astype(float)
     if not task_model.intervention_model.causal_model.discrete_treatment:
         df[intervention_col] = df[intervention_col].astype(float)
         if new_intervention_col in df: