fix: inverse of categorical-to-numerical conversion
- Fixed an error where the -1 sentinel was being incorrectly handled when performing the inverse of the categorical-to-numerical conversion
- Formatted with Black

actableai/utils/categorical_numerical_convert.py
@@ -3,14 +3,15 @@ import numpy as np
 
 from actableai.utils import get_type_special
 
+
 def get_categorical_columns(df):
     # Check which columns are not integers or floats, and assume they are categorical features to be converted
     cols_list = []
     for column in df.columns:
         col_type = get_type_special(df[column])
-        if col_type=='category':
+        if col_type == "category":
             cols_list.append(column)
-    
+
     return cols_list
 
 
@@ -41,7 +42,7 @@ def convert_categorical_to_num(df, inplace=False):
     return df, uniques_all
 
 
-def inverse_convert_categorical_to_num(df, uniques, feat_name=None, inplace=False):
+def inverse_convert_categorical_to_num(df_new, uniques, feat_name=None):
     """
     Convert numerical values back to their original categorical values.
 
@@ -51,7 +52,6 @@ def inverse_convert_categorical_to_num(df, uniques, feat_name=None, inplace=Fals
     df (pandas DataFrame): The DataFrame containing the numerical values to be converted back to categorical.
     uniques (dict): A dictionary containing the unique values for each column.
     feat_name (str, optional): The name of a specific feature to be converted. If None, all features in the DataFrame will be converted.
-    inplace (bool, optional): Whether to perform modifications to df in-place
 
     Returns:
     df (pandas DataFrame): The modified DataFrame with numerical values converted back to categorical values.
@@ -60,14 +60,13 @@ def inverse_convert_categorical_to_num(df, uniques, feat_name=None, inplace=Fals
     ValueError: If the feature name (column) is not in the DataFrame.
     """
 
-    if not inplace:
-        df = df.copy()
+    df = df_new.copy()
 
     if feat_name == None:  # Iterate over ALL features
         for c in uniques.keys():
-            df[c] = uniques[c].take(df[c])
+            df[c] = uniques[c].take(df_new[c])
             df.loc[
-                df[c] == -1, c
+                df_new[c] == -1, c
             ] = (
                 np.nan
             )  # NaN values are indicated with the -1 sentinel; replace them with NaN
@@ -75,9 +74,9 @@ def inverse_convert_categorical_to_num(df, uniques, feat_name=None, inplace=Fals
     else:  # Only one feature
         c = feat_name
         if c in df.columns:
-            df[c] = uniques[c].take(df[c])
+            df[c] = uniques[c].take(df_new[c])
             df[
-                df[c] == -1
+                df_new[c] == -1
             ] = (
                 np.nan
             )  # NaN values are indicated with the -1 sentinel; replace them with NaN